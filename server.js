a2bd237fcc3df32d7bec91499a3106d5e0f1af7e
shabiKi <shoaebkiyani@yahoo.com>
2022-05-01 08:12:58 +0300

    package update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
+const cors = require("cors");
+const router = jsonServer.router("./data/db.json");
 const middleware = jsonServer.defaults({
   static: "./build",
 });
@@ -9,11 +10,12 @@ const port = process.env.PORT || 8000;
 server.use(middleware);
 server.use(
   jsonServer.rewriter({
-    "/blog/*": "/$1",
+    "/blogs/*": "/$1",
   })
 );
 
 server.use(router);
+server.use(cors());
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
