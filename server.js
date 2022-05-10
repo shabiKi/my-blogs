989cbd02600bb5f91fd0b45c4733048a0746df9f
shabiKi <shoaebkiyani@yahoo.com>
2022-05-10 04:23:35 +0300

    package update

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const cors = require("cors");
-const router = jsonServer.router("./data/db.json");
-const middlewares = jsonServer.defaults({
-  static: "./build",
-});
+const path = require("path");
+const express = require("express");
+const router = jsonServer.router("../data/db.json");
+const middlewares = jsonServer.defaults();
+// const middlewares = jsonServer.defaults({
+//   static: "./build",
+// });
 
 const port = process.env.PORT || 8000;
-server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
+server.use("/db", middlewares, router);
+// server.use(
+//   jsonServer.rewriter({
+//     "/api/*": "/$1",
+//   })
+// );
 
-server.use(router);
+// server.use(router);
+server.use(express.static(path.join(__dirname, "build")));
 server.use(cors());
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
