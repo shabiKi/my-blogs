2d6387b641d0c8891ae6bb2d0da279c305cd5eed
shabiKi <shoaebkiyani@yahoo.com>
2022-06-02 09:44:30 +0300

    unwanted files removed

--- src/Home.js
+++ src/Home.js
@@ -3,18 +3,20 @@ import useFetch from "./useFetch";
 
 const Home = () => {
   const devEnv = process.env.NODE_ENV !== "production";
-  //const { REACT_APP_DEV_URL, REACT_APP_PROD_URL } = process.env;
+  const { REACT_APP_DEV_URL, REACT_APP_PROD_URL } = process.env;
   const {
     data: blogs,
     isLoading,
     error,
-  } = useFetch(
-    `${
-      devEnv
-        ? "http://localhost:8000/blogs"
-        : "https://blogsapp1.herokuapp.com/blogs"
-    }`
-  );
+  } = useFetch(`${devEnv ? REACT_APP_DEV_URL : REACT_APP_PROD_URL}`);
+
+  //   useFetch(
+  //   `${
+  //     devEnv
+  //       ? "http://localhost:8000/blogs"
+  //       : "https://blogsapp1.herokuapp.com/blogs"
+  //   }`
+  // );
 
   return (
     <div className="home">
